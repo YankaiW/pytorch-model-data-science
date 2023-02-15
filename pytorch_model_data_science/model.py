a6714945308f2f95d994f9e468a9d2d0ebd92d84
YankaiW <38786651+YankaiW@users.noreply.github.com>
2023-02-15 17:38:56 +0100

    Add some models to the repo (#3)
    In order to use this repo as the base source for PyTorch DS work,
    to add some basic model objects including,
    
    * PyTorchEstimator: to generate sklearn estimator from a PyTorch
      model.
    * DNNNetRegressor: a basic DNN regression model by PyTorch.
    * DNNNetClassifier: a basic DNN classification model by PyTorch.
    
    And at the same time, some necessary dependencies are also
    installed.

---
+++ pytorch_model_data_science/model.py
@@ -0,0 +1,147 @@
+"""The model used for defining PyTorch models and the following sklearn 
+estimator
+"""
+
+from typing import Optional
+
+import numpy as np
+import torch
+from sklearn import base, metrics
+from torch import nn
+
+
+class PyTorchEstimator(base.BaseEstimator, base.RegressorMixin):
+    """The class to define a sklearn estimator from PyTorch model"""
+
+    def __init__(self, model: nn.Module) -> None:
+        """Constructor
+
+        Parameters
+        ----------
+        model: nn.Module
+            the model from PyTorch
+        """
+        self.model = model
+
+    def fit(self, X: np.ndarray, y: Optional[np.ndarray] = None) -> None:
+        """Simulates fit process
+
+        Parameters
+        ----------
+        X: np.ndarray
+            the feature matrix for training
+        y: np.ndarray, default None
+            the target matrix for training
+        """
+        pass
+
+    def predict(self, X: np.ndarray, y: Optional[np.ndarray] = None) -> np.ndarray:
+        """Prediction for regression
+
+        Parameters
+        ----------
+        X: np.ndarray
+            the feature matrix
+        y: np.ndarray, default None
+            the target martix
+
+        Returns
+        -------
+        np.ndarray
+            the predicted array
+        """
+        return self.model(torch.Tensor(X)).detach().numpy().flatten()
+
+    def predict_proba(
+        self, X: np.ndarray, y: Optional[np.ndarray] = None
+    ) -> np.ndarray:
+        """Prediction for classification
+
+        Parameters
+        ----------
+        X: np.ndarray
+            the feature matrix
+        y: np.ndarray, default None
+            the target martix
+
+        Returns
+        -------
+        np.ndarray
+            the predicted array
+        """
+        return self.model(torch.Tensor(X)).detach().numpy()
+
+    def score(
+        self, X: np.ndarray, y: np.ndarray, sample_weight: Optional[np.ndarray] = None
+    ) -> float:
+        """Obtains MSE score
+
+        Parameters
+        ----------
+        X: np.ndarray
+            the feature matrix
+        y: np.ndarray
+            the real target matrix
+        sample_weight: np.ndarray, default None
+            the sample weight array
+
+        Returns
+        -------
+        float
+            the scoare
+        """
+        y_pred = self.predict(X)
+        return metrics.mean_squared_error(y, y_pred)
+
+
+# linear network for regression
+class DNNNetRegressor(nn.Module):
+    def __init__(
+        self,
+        input_size: int,
+        l1: int = 512,
+        l2: int = 128,
+        l3: int = 64,
+    ) -> None:
+        super(DNNNetRegressor, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(input_size, l1),
+            nn.ReLU(),
+            nn.Linear(l1, l2),
+            nn.ReLU(),
+            nn.Linear(l2, l3),
+            nn.ReLU(),
+            nn.Linear(l3, 1),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        return self.linear_relu_stack(x)
+
+
+# linear network for classification
+class DNNNetClassifier(nn.Module):
+    def __init__(
+        self,
+        input_size: int,
+        l1: int = 512,
+        l2: int = 128,
+        l3: int = 64,
+    ) -> None:
+        super(DNNNetClassifier, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(input_size, l1),
+            nn.ReLU(),
+            nn.Linear(l1, l2),
+            nn.ReLU(),
+            nn.Linear(l2, l3),
+            nn.ReLU(),
+            nn.Linear(l3, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        return self.linear_relu_stack(x)
