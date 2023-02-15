4c0d15176137768eba81e795d1f3e762101f6be1
YankaiW <38786651+YankaiW@users.noreply.github.com>
2023-02-15 17:39:47 +0100

    The functions to train and validate PyTorch models (#4)
    In this PR, the functions used to train and validate PyTorch
    models are added, which can make model,
    
    * Learn from each batch data when training.
    * Output metrics when test on test data.
    
    Co-authored-by: Yankai Wang <yankai.wang@momox.biz>

---
+++ pytorch_model_data_science/validation.py
@@ -0,0 +1,62 @@
+"""The module to get test metrics from the trained model
+"""
+
+from typing import Any, Tuple
+
+import torch
+from sklearn import metrics
+from torch.utils.data import DataLoader
+
+
+def test_classifier_accuracy(
+    network: torch.nn.Module,
+    dataloader: DataLoader,
+    loss_fn: Any,
+    model_type: str = "DNN",
+) -> Tuple[float, float, float, float]:
+    """Tests trained model
+
+    Parameters
+    ----------
+    network: torch.nn.Module
+        the trained pytorch model
+    dataloader: DataLoader
+        the dataloader for testing
+    loss_fn:
+        loss function
+    model_type: str, default DNN
+        the name of the model
+
+    Returns
+    -------
+    float
+        loss value
+    float
+        precision
+    float
+        recall
+    float
+        f1 score
+    """
+    # transform data
+    test_data = dataloader.dataset[:][0]
+    if model_type in ["CNN", "LSTM"]:
+        test_data = test_data[:, None, :]
+    network.eval()
+    with torch.no_grad():
+        pred = network(test_data)
+        loss = loss_fn(pred, dataloader.dataset[:][1]).item()
+    # metrics
+    precision = metrics.precision_score(
+        dataloader.dataset[:][1].numpy().flatten(),
+        pred.detach().numpy().flatten() > 0.5,
+    )
+    recall = metrics.recall_score(
+        dataloader.dataset[:][1].numpy().flatten(),
+        pred.detach().numpy().flatten() > 0.5,
+    )
+    f1 = metrics.f1_score(
+        dataloader.dataset[:][1].numpy().flatten(),
+        pred.detach().numpy().flatten() > 0.5,
+    )
+    return loss, precision, recall, f1
