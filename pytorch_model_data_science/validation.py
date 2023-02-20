39ea1ffdecf5a7768b3a8890ef3993cd7b3c6d02
YankaiW <38786651+YankaiW@users.noreply.github.com>
2023-02-20 15:12:33 +0100

    Add the function to calculate accuracies for regression model (#14)

--- pytorch_model_data_science/validation.py
+++ pytorch_model_data_science/validation.py
@@ -60,3 +60,58 @@ def test_classifier_accuracy(
         pred.detach().numpy().flatten() > 0.5,
     )
     return loss, precision, recall, f1
+
+
+def test_regressor_accuracy(
+    network: torch.nn.Module,
+    dataloader: DataLoader,
+    loss_fn: Any,
+    model_type: str = "DNN",
+) -> Tuple[float, float, float, float]:
+    """Tests trained model
+
+    Parameters
+    ----------
+    network: torch.nn.Module
+        the trained pytorch model
+    dataloader: DataLoader
+        the dataloader for testing
+    loss_fn:
+        loss function
+    model_type: str, default DNN
+        the name of the model
+
+    Returns
+    -------
+    float
+        loss value
+    float
+        precision
+    float
+        recall
+    float
+        f1 score
+    """
+    # transform data
+    test_data = dataloader.dataset[:][0]
+    if model_type in ["CNN", "LSTM"]:
+        test_data = test_data[:, None, :]
+
+    network.eval()
+    with torch.no_grad():
+        pred = network(test_data)
+        loss = loss_fn(pred, dataloader[:][1]).item()
+    # metrics
+    mae = metrics.mean_absolute_error(
+        dataloader[:][1].numpy().flatten(),
+        pred.detach().numpy().flatten(),
+    )
+    mse = metrics.mean_squared_error(
+        dataloader[:][1].numpy().flatten(),
+        pred.detach().numpy().flatten(),
+    )
+    r2 = metrics.r2_score(
+        dataloader[:][1].numpy().flatten(),
+        pred.detach().numpy().flatten(),
+    )
+    return loss, mae, mse, r2
